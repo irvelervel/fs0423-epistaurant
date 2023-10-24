328c749edb4f35a388b55cd5d7e7083f05507e98
Stefano Casasola <ste.casasola@gmail.com>
2023-10-24 11:00:09 +0200

    Adds custom navbar component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import logo from './logo.svg'
 import './App.css'
 // importiamo il foglio CSS di bootstrap, UNA VOLTA SOLA in App.js
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Badge } from 'react-bootstrap'
+
+// importiamo i componenti da noi scritti
+// senza { } perchè tutti i miei componenti li esporto come "default"
+import CustomNavbar from './components/CustomNavbar'
 
 // che cos'è un componente React a funzione?
 // È una funzione che ritorna del JSX
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <button className="btn btn-primary">BOTTONE PRIMARY</button> */}
-        <Button variant="primary" size="sm" className="mt-5">
-          Primary
-        </Button>
-        {/* <span class="badge bg-secondary">New</span> */}
-        <Badge bg="secondary" pill={true}>
-          New
-        </Badge>
-        <label htmlFor="name">Nome</label>
-        <input type="text" id="name" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* voglio aggiungere un sottotitolo alla mia navbar, personalizzato! */}
+      <CustomNavbar additionalText="I migliori piatti del web!" />
     </div>
   )
 }
